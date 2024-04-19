d20e9d6dd6f6649fadf7f6e092416c631d71a09c
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-04-19 22:04:41 +0500

    form datas is being gotten

--- src/components/extra-components/add-component.js
+++ src/components/extra-components/add-component.js
@@ -1,23 +1,105 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function AddComponent({}) {
+    const [ name, setName ] = useState('');
+    const [ user, setUser ] = useState('');
+    const [ pass, setPass] = useState('');
+    const [ phone, setPhone ] = useState('');
+    const [ role, setRole] = useState('');
+    const [ status, setStatus] = useState('');
+    const [ err, setErr] = useState({
+        nameErr: false,
+        userErr: false,
+        passErr: false,
+        phoneErr: false,
+        roleErr: false,
+        statusErr: false,
+    });
+
+    const phoneGet = (e) => {
+        if(e.target.value.length >= 10){
+            setPhone(phone);
+        }
+        else if(phone.length <= 9){
+            setPhone(e.target.value);
+        }
+    }
+    const submitter = (e) => {
+        e.preventDefault();
+        // console.log();
+        if (phone.length < 9){
+            setErr({...err, phoneErr: true});
+        } else{
+            const newObj = {
+                name: name,
+                userName: user,
+                password: pass,
+                phone: phone,
+                role: role,
+                status: status
+            }
+            setPhone("");
+            console.log(newObj);
+            e.target.reset();
+        }
+    }
     return (
         <div className="container-fluid create-part">
-            <div className="row">
+            <form className="row" onSubmit={(e) => {submitter(e)}}>
                 <div className="col-md-6">
+                    <button className="btn btn-primary">click on it</button>
+
                     <p className="my-label">Full name</p>
-                    <input type="text" className="my-input"/>
+                    <input
+                        type="text"
+                        className={err.nameErr?"my-input border border-danger": "my-input"}
+                        onChange={
+                        (e) => {
+                        setName(e.target.value)
+                        }}
+                    />
+
                     <p className="my-label">Username</p>
-                    <input type="text" className="my-input"/>
+                    <input
+                        type="text"
+                        className={err.userErr?"my-input border border-danger": "my-input"}
+                        onChange={
+                        (e) => {
+                        setUser(e.target.value)
+                        }}
+                    />
+
                     <p className="my-label">Password</p>
-                    <input type="password" className="my-input"/>
+                    <input
+                        type="password"
+                        className={err.passErr?"my-input border border-danger": "my-input"}
+                        onChange={
+                        (e) => {
+                        setPass(e.target.value)
+                        }}
+                    />
                 </div>
+
                 <div className="col-md-6">
+
                     <p className="my-label">Phone number</p>
-                    <input type="number" placeholder="+998" className="my-input"/>
+                    <input
+                        type="number"
+                        placeholder="+998"
+                        className={err.phoneErr?"border border-danger my-input":"my-input"}
+                        onChange={
+                        (e)=>{phoneGet(e)
+                        }}
+                        value={phone}
+                    />
+
                     <p className="my-label">Role</p>
-                    <select name="" id="" className="my-input">
-                        <option value="">Default</option>
+                    <select
+                        className={err.roleErr?"my-input border border-danger": "my-input"}
+                        onChange={
+                        (e) => {setRole(e.target.value)
+                        }}>
+                        <option value="nothing">Default</option>
                         <option value="admin">ADMIN</option>
                         <option value="transport">TRANSPORT</option>
                         <option value="operator">OPERATOR</option>
@@ -27,14 +109,18 @@ function AddComponent({}) {
                     </select>
 
                     <p className="my-label">Status</p>
-                    <select name="" id="" className="my-input">
-                        <option value="">Default</option>
+                    <select
+                        className={err.roleErr?"my-input border border-danger": "my-input"}
+                        onChange={
+                        (e)=>{setStatus(e.target.value)
+                        }}>
+                        <option value="nothing">Default</option>
                         <option value="active">Active</option>
                         <option value="inactive">Inactive</option>
                         <option value="terminated">Terminated</option>
                     </select>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
